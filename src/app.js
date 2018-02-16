baba79c386c096032dca4c60bf4f12e53eaf66e0
Logan Wood <lwood3499@gmail.com>
2018-02-15 20:04:18 -0600

    first expensify commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+// install --> import --> use
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<p>This is Expensify!</p>, document.getElementById('app'));
+
