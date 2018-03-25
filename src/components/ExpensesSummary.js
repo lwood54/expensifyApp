b0d4a57c11ee418217252f45c61e94fb44fffd28
Logan Wood <lwood3499@gmail.com>
2018-03-25 16:48:35 -0500

    extra formatting of ExpensesSummary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -4,15 +4,21 @@ import selectExpenses from '../selectors/expenses';
 import getExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
 
-export const ExpensesSummary = props => (
-  <div>
-    <h4>There are {props.expenses.length} expenses.</h4>
-    <h5>
-      Total of displayed expenses:{' '}
-      {numeral(props.expensesTotal / 100).format('$0,0.00')}
-    </h5>
-  </div>
-);
+export const ExpensesSummary = props => {
+  const expenseWord = props.expenses.length === 1 ? 'expense' : 'expenses';
+  const isVare = props.expenses.length === 1 ? 'is' : 'are';
+  return (
+    <div>
+      <h4>
+        There {isVare} {props.expenses.length} {expenseWord}.
+      </h4>
+      <h5>
+        Total {expenseWord}:{' '}
+        {numeral(props.expensesTotal / 100).format('$0,0.00')}
+      </h5>
+    </div>
+  );
+};
 
 const mapStateToProps = state => ({
   expenses: selectExpenses(state.expenses, state.filters),
