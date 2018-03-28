d96eaa416c0a4cab03fde40b91d4f766ee99a3a6
Logan Wood <lwood3499@gmail.com>
2018-03-27 20:26:12 -0500

    intro to subscribing to firebase data change and accessing data

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,38 +13,95 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database
-  .ref()
-  .set({
-    name: 'Logan Wood',
-    age: 36,
-    isMarried: true,
-    location: {
-      city: 'Fort Worth',
-      country: 'United States'
-    }
-  })
-  .then(() => {
-    console.log('Data is saved');
-  })
-  .catch(error => {
-    console.log('This failed.', error);
+const onValueChange = database.ref().on(
+  'value',
+  snapshot => {
+    console.log(snapshot.val());
+    let name = snapshot.val().name;
+    let job = snapshot.val().job;
+    let company = snapshot.val().company;
+    console.log(`${name} is a ${job} at ${company}`);
+  },
+  e => {
+    console.log('Error with data fetching: ', e);
+  }
+);
+
+setTimeout(() => {
+  database.ref().update({
+    name: 'Tiffany Wood',
+    job: 'AVID Teacher',
+    company: 'Adams MS'
   });
+}, 5000);
 
-// database.ref().set('This is my data.');
+// const onValueChange = database.ref().on(
+//   'value',
+//   snapshot => {
+//     console.log(snapshot.val());
+//   },
+//   e => {
+//     console.log('Error with data fetching: ', e);
+//   }
+// );
 
-// database.ref('age').set(37);
-// database.ref('location/city').set('Dallas');
+// setTimeout(() => {
+//   database.ref('age').set(29);
+// }, 3500);
 
-database
-  .ref('attributes')
-  .set({
-    height: '6ft 8in',
-    weight: '280lbs'
-  })
-  .then(() => {
-    console.log('Attributes have been saved.');
-  })
-  .catch(e => {
-    console.log('Current error: ', e);
-  });
+// setTimeout(() => {
+//   database.ref().off('value', onValueChange); // not in his video, but got error and checked
+// }, 7000); // docs, now this works.
+
+// setTimeout(() => {
+//   database.ref('age').set(36);
+// }, 10500);
+
+// database
+//   .ref('location/city')
+//   .once('value')
+//   .then(snapshot => {
+//     const val = snapshot.val();
+//     console.log(val);
+//   })
+//   .catch(e => {
+//     console.log('Error fetching data: ', e);
+//   });
+
+// database
+//   .ref()
+//   .set({
+//     name: 'Logan Wood',
+//     age: 36,
+//     stressLevel: 6,
+//     job: {
+//       title: 'front-end web developer',
+//       company: 'Google'
+//     },
+//     location: {
+//       city: 'Fort Worth',
+//       country: 'United States'
+//     }
+//   })
+//   .then(() => {
+//     console.log('Data is saved');
+//   })
+//   .catch(error => {
+//     console.log('This failed.', error);
+//   });
+
+//   database.ref().update({
+//     stressLevel: 9,
+//     'job/company': 'Amazon',
+//     'location/city': 'Seattle'
+//   });
+
+// database
+//   .ref('isMarried')
+//   .remove()
+//   .then(() => {
+//     console.log('prop has been removed');
+//   })
+//   .catch(e => {
+//     console.log('error occurred: ', e);
+//   });
