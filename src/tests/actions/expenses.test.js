1d1ec2b9fb0db2c4ad817db8de5a1e42f8c96e6d
Logan Wood <lwood3499@gmail.com>
2018-04-03 21:29:09 -0500

    used Promise chaining and async testing on expense.test.js

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -7,6 +7,7 @@ import {
   removeExpense
 } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
+import database from '../../firebase/firebase';
 
 const createMockStore = configureMockStore([thunk]);
 
@@ -43,15 +44,55 @@ test('should add expense to database and store', done => {
     description: 'Mouse',
     amount: 30000,
     note: 'This one is better',
-    createAt: 1000
+    createdAt: 1000
   };
-  store.dispatch(startAddExpense(expenseData)).then(() => {
-    expect(1).toBe(1);
-    done();
-  });
+  store
+    .dispatch(startAddExpense(expenseData))
+    .then(() => {
+      const actions = store.getActions();
+      expect(actions[0]).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+          id: expect.any(String),
+          ...expenseData
+        }
+      });
+
+      return database.ref(`expenses/${actions[0].expense.id}`).once('value');
+    })
+    .then(snapshot => {
+      expect(snapshot.val()).toEqual(expenseData);
+      done();
+    });
 });
 
-test('should add expense with defaults to database and store', () => {});
+test('should add expense with defaults to database and store', done => {
+  const store = createMockStore({});
+  const expenseDefaults = {
+    description: '',
+    amount: 0,
+    note: '',
+    createdAt: 0
+  };
+  store
+    .dispatch(startAddExpense({}))
+    .then(() => {
+      const actions = store.getActions();
+      expect(actions[0]).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+          id: expect.any(String),
+          ...expenseDefaults
+        }
+      });
+
+      return database.ref(`expenses/${actions[0].expense.id}`).once('value');
+    })
+    .then(snapshot => {
+      expect(snapshot.val()).toEqual(expenseDefaults);
+      done();
+    });
+});
 
 // test('should setup add expense action object with default values', () => {
 //   const action = addExpense();
